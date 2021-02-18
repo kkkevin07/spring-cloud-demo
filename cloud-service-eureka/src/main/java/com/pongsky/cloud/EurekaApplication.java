e11d220e6680fa5322c07a69761a3b3edb891cf5
彭森豪 <kelry@vip.qq.com>
2021-02-18 10:05:18 +0800

    feat(cloud-eureka): 增加 eureka 服务

---
+++ cloud-service-eureka/src/main/java/com/pongsky/cloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.pongsky.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author pengsenhao
+ * @create 2021-02-18
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
